453e3caaf7a1a225294e79f819defd7acc23c564
Jonathan <jgimmink@avans.nl>
2017-03-10 15:42:53 +0100

    Imported project

---
+++ HorrorRockSimulator/Assets/Scripts/Character/CamMouseLook.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CamMouseLook: MonoBehaviour
+{
+	[SerializeField]
+	private float sensitivity = 5.0f;
+	[SerializeField]
+	public float smoothing = 2.0f;
+
+	private Vector2 mouseLook;
+	private Vector2 smoothV;
+	private GameObject character;
+
+	private void Start()
+	{
+		character = transform.parent.gameObject;
+	}
+
+	private void Update()
+	{
+		Vector2 md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+		md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+		smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+		mouseLook += smoothV;
+		mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+	}
+}
---
+++ HorrorRockSimulator/Assets/Scripts/Character/CharacterMover.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMover: MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 4.0f;
+	[SerializeField]
+	private float jumpForce = 4.0f;
+
+	private Vector2 rot;
+	private bool isGrounded;
+	private Rigidbody characterRigidbody;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		characterRigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		float translation = Input.GetAxis("Vertical") * speed;
+		float straffe = Input.GetAxis("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate(straffe, 0, translation);
+
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+			isGrounded = false;
+		}
+	}
+
+	private void OnCollisionStay()
+	{
+		isGrounded = true;
+	}
+}
